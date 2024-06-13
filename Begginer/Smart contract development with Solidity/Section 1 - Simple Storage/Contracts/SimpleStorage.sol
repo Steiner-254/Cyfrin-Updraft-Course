01946c358a16aa47c82016c590e3618d77929042
steiner-254 <steineralvin3@gmail.com>
2024-06-13 21:19:33 +0300

    updating the smart contract

--- Begginer/Smart contract development with Solidity/Section 1 - Simple Storage/Contracts/SimpleStorage.sol
+++ Begginer/Smart contract development with Solidity/Section 1 - Simple Storage/Contracts/SimpleStorage.sol
@@ -2,5 +2,11 @@
 pragma solidity 0.8.18; //stating our version
 
 contract SimpleStorage {
-    
+    // Basic Types: boolean, uint, int, address, bytes
+    bool hasFavoriteNumber = true;
+    uint256 favoriteNumber = 88;
+    string favoriteNumberInText = "eighty-eight";
+    int256 favoriteInt= -88;
+    address myAddress = 0x224225D920B2193c22313f9FE0a63571aE75B852;
+    bytes32 favoriteBytes32 = "cat";
 }
