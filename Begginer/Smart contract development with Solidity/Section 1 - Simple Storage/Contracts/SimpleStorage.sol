c5c3fc398b25c6b7ef095932a471a8c4328e3ca5
steiner-254 <steineralvin3@gmail.com>
2024-06-16 13:19:19 +0300

    update

--- Begginer/Smart contract development with Solidity/Section 1 - Simple Storage/Contracts/SimpleStorage.sol
+++ Begginer/Smart contract development with Solidity/Section 1 - Simple Storage/Contracts/SimpleStorage.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.18; //stating our version
 contract SimpleStorage {
     // Basic Types: boolean, uint, int, address, bytes
     // get's initialized to 0 if no value given
-    uint256 favoriteNumber;
+    uint256 public favoriteNumber;
 
     function store(uint256 _favoriteNumber) public {
         favoriteNumber = _favoriteNumber;
