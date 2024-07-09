a00cc93eaa396db505099ff4a942a67bb9bed4c8
steiner-254 <steineralvin3@gmail.com>
2024-07-09 14:52:57 +0300

    named import updates

--- Begginer/Smart contract development with Solidity/Section 2 - Storage Factory/Contracts/StorageFactory.sol
+++ Begginer/Smart contract development with Solidity/Section 2 - Storage Factory/Contracts/StorageFactory.sol
@@ -2,7 +2,9 @@
 pragma solidity ^0.8.18;
 
 // using "import" to import contents from other files
-import "./SimpleStorage.sol";
+// named import -> import {SimpleStorage} from "./SimpleStorage.sol"
+
+import {SimpleStorage} from "./SimpleStorage.sol";
 
 contract StorageFactory {
 
