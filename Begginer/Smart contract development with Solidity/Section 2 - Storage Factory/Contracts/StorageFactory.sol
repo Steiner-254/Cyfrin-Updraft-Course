108a3cbaf51647dcb1aaa7b9f0cc8ac121e63b8b
steiner-254 <steineralvin3@gmail.com>
2024-07-09 13:36:24 +0300

    update

--- Begginer/Smart contract development with Solidity/Section 2 - Storage Factory/Contracts/StorageFactory.sol
+++ Begginer/Smart contract development with Solidity/Section 2 - Storage Factory/Contracts/StorageFactory.sol
@@ -38,7 +38,11 @@ contract SimpleStorage {
 
 contract StorageFactory {
 
+    // uint256 public favoriteNumber
+    // type visibility name
+    SimpleStorage public simpleStorage;
+
     function createSimpleStorageContract() public {
-        // How does the storage factory know what simple storage looks like?
+        simpleStorage = new SimpleStorage();
     }
 }
