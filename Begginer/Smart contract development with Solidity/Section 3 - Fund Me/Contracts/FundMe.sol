6780fa25281d8fc11d579d3de68fb4eab5a94861
steiner-254 <steineralvin3@gmail.com>
2024-07-12 15:35:37 +0300

    update on teh smart contract

--- Begginer/Smart contract development with Solidity/Section 3 - Fund Me/Contracts/FundMe.sol
+++ Begginer/Smart contract development with Solidity/Section 3 - Fund Me/Contracts/FundMe.sol
@@ -7,11 +7,13 @@ pragma solidity ^0.8.18;
 
 contract FundMe {
 
+    uint256 minimumUsd = 5;
+
     function fund() public payable {
         // Allow users to send $$
         // Have a minimum $$ spent
         // How do we send eth to this contract?
-        require(msg.value > 1e18, "Didn't Send Enough Eth"); // 1e18 = 1ETH = 1000000000000000000 Wei = 1000000000 Gwei
+        require(msg.value >= minimumUsd, "Didn't Send Enough Eth"); // 1e18 = 1ETH = 1000000000000000000 Wei = 1000000000 Gwei
     }
 
     // function withdraw() public {}
