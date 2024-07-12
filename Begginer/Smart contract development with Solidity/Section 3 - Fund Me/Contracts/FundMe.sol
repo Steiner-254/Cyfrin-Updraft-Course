b94748f32065f81723eedaef273b8319f1d0ad4c
steiner-254 <steineralvin3@gmail.com>
2024-07-12 14:13:16 +0300

    updates on the reverts

--- Begginer/Smart contract development with Solidity/Section 3 - Fund Me/Contracts/FundMe.sol
+++ Begginer/Smart contract development with Solidity/Section 3 - Fund Me/Contracts/FundMe.sol
@@ -6,10 +6,14 @@
 pragma solidity ^0.8.18;
 
 contract FundMe {
+
+    uint256 public myValue = 1;
+
     function fund() public payable {
         // Allow users to send $$
         // Have a minimum $$ spent
         // How do we send eth to this contract?
+        myValue = myValue +2;
         require(msg.value > 1e18, "Didn't Send Enough Eth"); // 1e18 = 1ETH = 1000000000000000000 Wei = 1000000000 Gwei
     }
 
