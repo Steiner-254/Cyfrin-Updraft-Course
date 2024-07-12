2a4afa57fcc676ccfdd98044b0fe67dd63b53e24
steiner-254 <steineralvin3@gmail.com>
2024-07-12 08:39:13 +0300

    update on the FundMe smart contract

--- Begginer/Smart contract development with Solidity/Section 3 - Fund Me/Contracts/FundMe.sol
+++ Begginer/Smart contract development with Solidity/Section 3 - Fund Me/Contracts/FundMe.sol
@@ -1,6 +1,6 @@
 // Get funds from users
 // Withdraw funds
-// Set a minimum funding value in USD
+// Set a minimum funding value in (USD)
 
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
