26e1afa8a3ee3e10c5cdf64e9e158602a809756c
steiner-254 <steineralvin3@gmail.com>
2024-07-16 20:54:20 +0300

    updates - similarity in the codes

--- Begginer/Smart contract development with Solidity/Section 3 - Fund Me/Contracts/SafeMathTester.sol
+++ Begginer/Smart contract development with Solidity/Section 3 - Fund Me/Contracts/SafeMathTester.sol
@@ -8,3 +8,16 @@ contract SafeMathTester {
         unchecked {bigNumber = bigNumber + 1;} // using the unchecked keyword
     }
 }
+
+// The Code Above Is Simmilar to the code below:
+
+// SPDX-License-Identifier: MIT
+// pragma solidity ^0.6.0;
+//
+// contract SafeMathTester {
+//    uint8 public bigNumber = 255; // This is unchecked
+//
+//    function add() public {
+//        bigNumber = bigNumber + 1;
+//    }
+//} 
