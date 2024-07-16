b235876da59c2aa55c2131d0ae4ee894b132c1fd
steiner-254 <steineralvin3@gmail.com>
2024-07-16 20:48:42 +0300

    update on the checked contract...preventing integer overflow

--- Begginer/Smart contract development with Solidity/Section 3 - Fund Me/Contracts/SafeMathTester.sol
+++ Begginer/Smart contract development with Solidity/Section 3 - Fund Me/Contracts/SafeMathTester.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.6.0;
+pragma solidity ^0.8.0;
 
 contract SafeMathTester {
-    uint8 public bigNumber = 255; // This is unchecked
+    uint8 public bigNumber = 255; // This is checked
 
     function add() public {
         bigNumber = bigNumber + 1;
