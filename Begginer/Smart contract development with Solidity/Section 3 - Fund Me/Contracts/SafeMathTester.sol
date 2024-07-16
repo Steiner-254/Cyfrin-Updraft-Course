cea0396d689d41831f3fc109955e18d87ce3853e
steiner-254 <steineralvin3@gmail.com>
2024-07-16 20:45:30 +0300

    updated

--- Begginer/Smart contract development with Solidity/Section 3 - Fund Me/Contracts/SafeMathTester.sol
+++ Begginer/Smart contract development with Solidity/Section 3 - Fund Me/Contracts/SafeMathTester.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.6.0;
 
 contract SafeMathTester {
-    uint8 public bigNumber = 255;
+    uint8 public bigNumber = 255; // This is unchecked
 
     function add() public {
         bigNumber = bigNumber + 1;
