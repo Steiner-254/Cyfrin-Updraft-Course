80d434814fa01bc32a2e852f5d1989355f959179
steiner-254 <steineralvin3@gmail.com>
2024-10-05 21:17:17 +0300

    updates

--- gpush.bat
+++ gpush.bat
@@ -1,3 +1,10 @@
+:: Steps To Create The Simple Automation Workflow Below:
+:: 1. Create a file with extension `.bat` -> gpush.bat
+:: 2. Copy the code below into the file/Add your own commands if you wish
+:: 3. Open the windows terminal on VS Code and run the command `.\gpush.bat` to execute the script - `gpush.bat` is our file name, feel free to cutomize
+:: 4. Successfully Automated
+:: -> Happy Development <3
+
 @echo off
 git add .
 git commit -m "updates"
