536321c4b6a1ba2b8fe102e16244e7a7dabcb89f
steiner-254 <steineralvin3@gmail.com>
2024-10-06 08:10:39 +0300

    updates

--- gpush.bat
+++ gpush.bat
@@ -1,4 +1,4 @@
-:: Steps To Create The Simple Automation Workflow Below:
+:: Steps To Create The Simple Automation Workflow Below - Works Only For Windows VS Code Terminal:
 :: 1. Create a file with extension `.bat` -> gpush.bat
 :: 2. Copy the code below into the file/Add your own commands if you wish
 :: 3. Open the windows terminal on VS Code and run the command `.\gpush.bat` to execute the script - `gpush.bat` is our file name, feel free to cutomize
